96e3c84c61f35488af7947a4171bb381f6b08071
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2022-04-25 12:03:43 +0430

    SITE_ID = 1

--- backend/settings.py
+++ backend/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Sites Pakckage
+SITE_ID = 1
