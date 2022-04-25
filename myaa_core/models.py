d005af437acf2711ed74121e3c83d73e26b03434
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2022-04-25 11:53:15 +0430

    Add WorkShop Model

--- myaa_core/models.py
+++ myaa_core/models.py
@@ -1,3 +1,15 @@
+from webbrowser import get
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class WorkShop(models.Model):
+    name = models.CharField(max_length=99)
+
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+
+    owner = get_user_model()
+    add_time = models.DateTimeField(auto_created=True)
+    edit_time = models.DateTimeField(auto_now=True)
